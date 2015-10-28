4dfaea149be002c261fa1d141faf8bed385f78dd
unknown <mfinke@9ZPFG12.olin.edu>
2015-10-28 15:07:48 -0400

    everything except deliverable 1

--- HW/HW4/register.v
+++ HW/HW4/register.v
@@ -15,3 +15,37 @@ input		clk
     end
 
 endmodule
+
+//Assignes d to q if clk and write enable are on
+module register32
+(
+output reg 	[31:0] q,
+input 		[31:0] d,
+input 		wrenable,
+input 		clk
+);
+
+	always @(posedge clk) begin
+		if(wrenable) begin
+			q = d;
+		end
+	end
+endmodule
+
+
+
+//Assignes 32'b0 to q if clk and write enable are on
+module register32zero
+(
+output reg 	[31:0] q,
+input 		[31:0] d,
+input 		wrenable,
+input 		clk
+);
+
+	always @(posedge clk) begin
+		if(wrenable) begin
+			q = 32'b0;
+		end
+	end
+endmodule
